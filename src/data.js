57b4528958d8f559f070e104271d06cb3c8ab18a
Ledio <L.Bode515@gmail.com>
2018-07-26 17:31:44 -0400

    stringify data from API

--- src/data.js
+++ src/data.js
@@ -1,19 +1,37 @@
-const startTime = new Date();
-let showArray = [];
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+let showObj = { shows: [] };
 
 getShows = (i) => {
   fetch(URL + i)
     .then((res) => res.json())
-    .then((shows) => [...showArray, shows])
-    .then((endTime) => {
-      const finalTime = new Date();
-      console.log((finalTime.getTime() - startTime.getTime()) / 1000);
-    });
+    .then((data) => (showObj.shows = showObj.shows.concat(data)));
 };
 
-const URL = 'http://api.tvmaze.com/shows?page=';
-for (let i = 0; i < 151; i++) {
+const URL = 'https://api.tvmaze.com/shows?page=';
+for (let i = 0; i < 152; i++) {
   getShows(i);
 }
 
-console.log(showArray);
+let showsJSON = JSON.stringify(showObj);
+// const URL = 'http://api.tvmaze.com/shows?page=';
+// let showObj = { shows: [] };
+
+// for (let i = -1; i < 152; i++) {
+//   fetch(URL + i)
+//     .then((res) => res.json())
+//     .then((data) => pushShows(data));
+// }
+// let results = JSON.stringify(showObj);
+// console.log(results);
+
+// pushShows = (data) => {
+//   debugger;
+//   showObj.shows.concat(data);
+// };
+
+
+
+
+
